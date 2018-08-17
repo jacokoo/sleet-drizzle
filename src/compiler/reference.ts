8152c947a0acdac5a797fdbbba6f137db7f0ad1e
Jaco Koo <jaco.koo@guyong.in>
2018-08-17 12:30:19 +0800

    handle static attribute in reference node

--- src/compiler/reference.ts
+++ src/compiler/reference.ts
@@ -1,14 +1,16 @@
 import { TagCompiler } from './tag'
 
 export class ReferenceCompiler extends TagCompiler {
+    staticAttributes: [string, any][] = []
 
     doCompile () {
         this.tag.attributes.forEach(it => this.doAttribute(it))
         this.context.factory('REF')
 
         const na = `'${this.tag.name}'`
+        const sa = this.staticAttributes.map(it => `['${it[0]}', ${it[1]}]`).join(', ')
         const id = this.tag.id ? `, '${this.tag.id}'` : ''
-        this.context.init(`const ${this.id} = REF(${na}${id})`)
+        this.context.init(`const ${this.id} = REF(${na}, [${sa}]${id})`)
         this.inits.forEach(it => this.context.init(it))
 
         this.compileChildren()
@@ -19,6 +21,12 @@ export class ReferenceCompiler extends TagCompiler {
             if (attr.namespace === 'on') return this.doEvent(attr)
             if (attr.namespace === 'action') return this.doAction(attr)
             throw new SyntaxError(`attribute namespace ${attr.namespace} in refernece node is not supported`)
+        }
+
+        const v = attr.value[0]
+        if (v.minor === 'boolean' || v.minor === 'quoted' || v.minor === 'number') {
+            this.staticAttributes.push([attr.name, v.minor === 'quoted' ? `'${v.value}'` : v.value])
+            return
         }
 
         this.doBind(attr)
